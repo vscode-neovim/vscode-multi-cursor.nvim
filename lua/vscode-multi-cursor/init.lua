fc768386df080fd2386c281c79c9be0479dfa7f2
wongxy <xiyao.wong@foxmail.com>
2023-10-19 16:35:32 +0800

    fix: auto setup if the user don't setup

--- lua/vscode-multi-cursor/init.lua
+++ lua/vscode-multi-cursor/init.lua
@@ -1,11 +1,15 @@
 local M = {}
 
+local is_set
+
 function M.setup(opts) require('vscode-multi-cursor.cursors').setup(opts) end
 
-_G._C = setmetatable(M, {
+return setmetatable(M, {
   __index = function(_, k)
+    if not is_set then
+      M.setup()
+      is_set = true
+    end
     return require('vscode-multi-cursor.cursors')[k] or require('vscode-multi-cursor.wrappers')[k]
   end,
 })
-
-return _C
